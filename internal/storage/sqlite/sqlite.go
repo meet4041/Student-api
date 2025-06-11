3fd8c4b63c75870017abbb6d0b9f7446ee9f1564
Meet Gandhi <meetgandhi4041@gmail.com>
2025-06-11 16:08:16 +0530

    add-students-list

--- internal/storage/sqlite/sqlite.go
+++ internal/storage/sqlite/sqlite.go
@@ -73,3 +73,28 @@ func (s *Sqlite) GetStudentById(id int64) (types.Student, error) {
 	return student, nil
 
 }
+
+func (s *Sqlite) GetStudents() ([]types.Student, error) {
+	stmt, err := s.Db.Prepare("SELECT id, name, email,age from students")
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+	rows, err := stmt.Query()
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var students []types.Student
+
+	for rows.Next() {
+		var student types.Student
+		err := rows.Scan(&student.ID, &student.Name, &student.Email, &student.Age)
+		if err != nil {
+			return nil, err
+		}
+		students = append(students, student)
+	}
+	return students, nil
+}
